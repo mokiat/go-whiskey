7a624baee0a804181fee08bc078db5ef5d7b3df2
Momchil Atanasov <m.momchil@gmail.com>
2014-11-26 20:56:36 +0200

    Add Math/Vec3 API

---
+++ math/vec3/vec3.go
@@ -0,0 +1,99 @@
+package vec3
+
+import "math"
+
+type Vec3 struct {
+	X float32
+	Y float32
+	Z float32
+}
+
+func (v Vec3) Null() bool {
+	return v == Vec3{}
+}
+
+func (v Vec3) Inverse() Vec3 {
+	return Vec3{-v.X, -v.Y, -v.Z}
+}
+
+func (v Vec3) IncCoords(x, y, z float32) Vec3 {
+	return Vec3{
+		v.X + x,
+		v.Y + y,
+		v.Z + z,
+	}
+}
+
+func (v Vec3) DecCoords(x, y, z float32) Vec3 {
+	return Vec3{
+		v.X - x,
+		v.Y - y,
+		v.Z - z,
+	}
+}
+
+func (v Vec3) Mul(amount float32) Vec3 {
+	return Vec3{
+		v.X * amount,
+		v.Y * amount,
+		v.Z * amount,
+	}
+}
+
+func (v Vec3) Div(amount float32) Vec3 {
+	return Vec3{
+		v.X / amount,
+		v.Y / amount,
+		v.Z / amount,
+	}
+}
+
+func (v Vec3) LengthSquared() float32 {
+	return v.X*v.X + v.Y*v.Y + v.Z*v.Z
+}
+
+func (v Vec3) Length() float32 {
+	return float32(math.Sqrt(float64(v.LengthSquared())))
+}
+
+func (v Vec3) Resize(desiredLength float32) Vec3 {
+	ratio := desiredLength / v.Length()
+	return v.Mul(ratio)
+}
+
+func (v Vec3) DistanceToCoords(x, y, z float32) float32 {
+	delta := v.DecCoords(x, y, z)
+	return delta.Length()
+}
+
+func (v Vec3) DistanceToVec3(other Vec3) float32 {
+	return v.DistanceToCoords(other.X, other.Y, other.Z)
+}
+
+func Null() Vec3 {
+	return Vec3{}
+}
+
+func BaseVectorX() Vec3 {
+	return Vec3{1.0, 0.0, 0.0}
+}
+
+func BaseVectorY() Vec3 {
+	return Vec3{0.0, 1.0, 0.0}
+}
+
+func BaseVectorZ() Vec3 {
+	return Vec3{0.0, 0.0, 1.0}
+}
+
+func DotProduct(a, b Vec3) float32 {
+	return a.X*b.X + a.Y*b.Y + a.Z*b.Z
+}
+
+func CrossProduct(a, b Vec3) Vec3 {
+	return Vec3{
+		a.Y*b.Z - a.Z*b.Y,
+		a.Z*b.X - a.X*b.Z,
+		a.X*b.Y - a.Y*b.X,
+	}
+}

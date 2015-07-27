fb4c005ae6e8beafb52c97ebca12d48ea4fde230
Momchil Atanasov <momchil.dev@gmail.com>
2015-07-27 22:26:52 +0300

    Remove graphics client API
    -> This has now been moved to go-whiskey-android

--- graphics/client/element.go
+++
@@ -1,8 +0,0 @@
-package client
-
-//go:generate counterfeiter -o client_fakes/fake_element.go ./ ElementClient
-
-type ElementClient interface {
-	DrawTriangles(indexCount, indexOffsetInBytes int) error
-	DrawLines(indexCount, indexOffsetInBytes int) error
-}

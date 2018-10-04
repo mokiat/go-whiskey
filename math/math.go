7ca861bbf96cf484f834bdb9a188f77413278b01
Momchil Atanasov <momchil.dev@gmail.com>
2018-10-04 23:15:57 +0300

    refactor(math): rename go math import to gomath

--- math/math.go
+++ math/math.go
@@ -1,34 +1,34 @@
 package math
 
 import (
-	intmath "math"
+	gomath "math"
 )
 
-const Pi = float32(intmath.Pi)
+const Pi = float32(gomath.Pi)
 
 func Abs32(value float32) float32 {
-	return float32(intmath.Abs(float64(value)))
+	return float32(gomath.Abs(float64(value)))
 }
 
 func Sin32(angle float32) float32 {
-	return float32(intmath.Sin(float64(angle)))
+	return float32(gomath.Sin(float64(angle)))
 }
 
 func Cos32(angle float32) float32 {
-	return float32(intmath.Cos(float64(angle)))
+	return float32(gomath.Cos(float64(angle)))
 }
 
 func Sqrt32(value float32) float32 {
-	return float32(intmath.Sqrt(float64(value)))
+	return float32(gomath.Sqrt(float64(value)))
 }
 
 func Signum32(value float32) float32 {
-	if intmath.Signbit(float64(value)) {
+	if gomath.Signbit(float64(value)) {
 		return -1.0
 	}
 	return 1.0
 }
 
 func Atan32(value float32) float32 {
-	return float32(intmath.Atan(float64(value)))
+	return float32(gomath.Atan(float64(value)))
 }
